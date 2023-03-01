19acc6cb6e33c69219d32fec140e83b3b841b80e
judemont <96385330+judemont@users.noreply.github.com>
2023-03-01 14:20:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from pynput import keyboard
 import pyautogui
 import threading
+import sys
 
-SENSIBILITY = 30
+if len(sys.argv) > 2:
+    if(sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+        print("Usage: python3 main.py [SENSIBILITY] \n Default sensitivity is 30")
+    else:
+        SENSIBILITY = int(sys.argv[1])
+
+else:
+    SENSIBILITY = 30
 
 
 def move_cursor(key):
